e729c371b148a363476259903a609e1e4a77d37e
simplegr33n <gcgolda@gmail.com>
2019-03-27 17:07:48 -0600

    added build in docs dir

---
+++ docs/precache-manifest.7f82f76b128772839f0cc50c115cb195.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5ece3763128ed876ff790e1b1fa23a5c",
+    "url": "/simplegr33n/grovv.xyz/static/media/corner-logo.5ece3763.png"
+  },
+  {
+    "revision": "e93be37ca01c1ae99aed",
+    "url": "/simplegr33n/grovv.xyz/static/js/runtime~main.6c55e746.js"
+  },
+  {
+    "revision": "d3e7d2d98dc6aedf25ea",
+    "url": "/simplegr33n/grovv.xyz/static/js/main.ab8a9e67.chunk.js"
+  },
+  {
+    "revision": "0042b15790fc5408033f",
+    "url": "/simplegr33n/grovv.xyz/static/js/2.efd19b3a.chunk.js"
+  },
+  {
+    "revision": "d3e7d2d98dc6aedf25ea",
+    "url": "/simplegr33n/grovv.xyz/static/css/main.fffd33f9.chunk.css"
+  },
+  {
+    "revision": "9a1b882e063e71a819cd0b53516f2899",
+    "url": "/simplegr33n/grovv.xyz/index.html"
+  }
+];
