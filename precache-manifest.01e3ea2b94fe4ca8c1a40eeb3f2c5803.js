007e5c82e0c06a19c8bebd3eb953e7bb4caa0d61
simplegr33n <gcgolda@gmail.com>
2019-04-29 17:51:34 -0600

    Updates

--- precache-manifest.01e3ea2b94fe4ca8c1a40eeb3f2c5803.js
+++ precache-manifest.01e3ea2b94fe4ca8c1a40eeb3f2c5803.js
@@ -8,23 +8,23 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.a8a9905a.js"
   },
   {
-    "revision": "4e60151e067769e704e9",
-    "url": "/static/js/main.5fd27f0e.chunk.js"
+    "revision": "730ae9f2fc263c3bad16",
+    "url": "/static/js/main.3f2aa37a.chunk.js"
   },
   {
     "revision": "3b2cf264ece43bef505b",
     "url": "/static/js/2.a9cc9832.chunk.js"
   },
   {
-    "revision": "4e60151e067769e704e9",
-    "url": "/static/css/main.0f8050de.chunk.css"
+    "revision": "730ae9f2fc263c3bad16",
+    "url": "/static/css/main.7470a3f0.chunk.css"
   },
   {
     "revision": "3b2cf264ece43bef505b",
     "url": "/static/css/2.a56e1b11.chunk.css"
   },
   {
-    "revision": "8c0014e7032f5215e4b31ed408a03855",
+    "revision": "a9faf8c956ab4f5eac9b8c01b7c04a9d",
     "url": "/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78fee78a3f32cd704407c4ea6db07f33.js"
+  "/precache-manifest.01e3ea2b94fe4ca8c1a40eeb3f2c5803.js"
 );
 
 workbox.clientsClaim();
