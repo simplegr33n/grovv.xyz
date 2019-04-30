b6fe88a4e86f9e77c8af071de457ca8b5794503b
simplegr33n <gcgolda@gmail.com>
2019-04-30 01:52:46 -0600

    changed timeline dot css id naming

--- src/components/main-content/GrowJournal/JournalTimelineButton.js
+++ src/components/main-content/GrowJournal/JournalTimelineButton.js
@@ -7,12 +7,24 @@ class JournalTimelineButton extends Component {
         super(props);
         this.state = {
             journalName: '',
-            journalID: this.props.journalID
+            journalID: this.props.journalID,
+            monthDayDate: ''
         };
 
     }
 
     componentDidMount() {
+
+        if (this.props.entry) {
+            var timelineDate = new Date(this.props.entry.datetime_true)
+            var shortMonth = (timelineDate.getMonth() + 1) + "-"
+            if (shortMonth.length === 2) {
+                shortMonth = "0" + shortMonth;
+            }
+            var shortDateVar = shortMonth + timelineDate.getDate()
+            this.setState({ monthDayDate: shortDateVar });
+
+        }
 
 
     }
@@ -23,27 +35,21 @@ class JournalTimelineButton extends Component {
 
     render() {
         var idVar = "Timeline-Dot-" + this.props.entry.grow_stage
+        var growStage = this.props.entry.grow_stage
+        var idVar = "Timeline-Dot-" + growStage.charAt(0).toUpperCase() + growStage.slice(1)
 
-        var timelineDate = new Date(this.props.entry.datetime_true)
-        var shortMonth = (timelineDate.getMonth() + 1) + "-"
-        if (shortMonth.length === 2) {
-            shortMonth = "0" + shortMonth;
-        }
-        var shortDateVar = shortMonth + timelineDate.getDate()
-
+        // Check if the button corresponds to the active entry
         var isActiveEntry = false;
         if (this.props.currentEntryID === this.props.entry.id) {
             isActiveEntry = true;
-            console.log("YIPers!")
         }
-
 
         return (
             <div>
                 {(() => {
                     if (isActiveEntry) {
                         return (
-                            <div className="Timeline-Dot-Connector"><div className="Dot-Connector-Text-rotate-180">{shortDateVar}</div></div>
+                            <div className="Timeline-Dot-Connector"><div className="Dot-Connector-Text-rotate-180">{this.state.monthDayDate}</div></div>
                         )
                     }
                 })()}
