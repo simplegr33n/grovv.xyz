62b906b81c0ce68e2b28e3b7bc5a8f1226cafcfa
simplegr33n <gcgolda@gmail.com>
2019-05-10 16:16:25 -0600

    created template app nav bar

--- src/App/app-bar/AppBar.js
+++ src/App/app-bar/AppBar.js
@@ -19,7 +19,45 @@ class AppBar extends Component {
         return (
 
             <div id="App-Bar">
+                <div id="App-Bar-Logo">
+                    growbase
+                </div>
+                <div className="App-Bar-Button">
+                    GROWS
+                </div>
+                <div className="App-Bar-Button-Grow">
+                    <div className="App-Bar-Button-Grow-Name">
+                        GG
+                    </div>
 
+                    <div className="App-Bar-Button-Grow-Temp">
+                        22C
+                        </div>
+                    <div className="App-Bar-Button-Grow-Humidity">
+                        34RH
+                        </div>
+
+                </div>
+                <div className="App-Bar-Button-Grow">
+                    <div className="App-Bar-Button-Grow-Name">
+                        VG
+                    </div>
+
+                    <div className="App-Bar-Button-Grow-Temp">
+                        27C
+                        </div>
+                    <div className="App-Bar-Button-Grow-Humidity">
+                        54RH
+                        </div>
+
+                </div>
+                <div className="App-Bar-Button">
+                    JRNLS
+                </div>
+                <div className="App-Bar-Filler-Div"></div>
+                <div className="App-Bar-Logout-Button">
+                    &#10162;
+                </div>
             </div>
 
         );
