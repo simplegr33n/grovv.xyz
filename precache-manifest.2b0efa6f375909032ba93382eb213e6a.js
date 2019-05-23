f83bb7a6040246cfa935619cd6680e302f66141e
simplegr33n <gcgolda@gmail.com>
2019-05-23 12:46:17 -0600

    Updates

---
+++ precache-manifest.2b0efa6f375909032ba93382eb213e6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fc9682389f3821381f69",
+    "url": "/static/js/main.ed3a2a2a.chunk.js"
+  },
+  {
+    "revision": "bb2664e2c5bfe02a2097",
+    "url": "/static/js/2.d21f7448.chunk.js"
+  },
+  {
+    "revision": "fc9682389f3821381f69",
+    "url": "/static/css/main.0a6b06b6.chunk.css"
+  },
+  {
+    "revision": "bb2664e2c5bfe02a2097",
+    "url": "/static/css/2.534c9e18.chunk.css"
+  },
+  {
+    "revision": "20e1160cfabc14e71e543e159fa0c9e2",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1886275a797438eeba367e88eff5affd.js"
+  "/precache-manifest.2b0efa6f375909032ba93382eb213e6a.js"
 );
 
 workbox.clientsClaim();
