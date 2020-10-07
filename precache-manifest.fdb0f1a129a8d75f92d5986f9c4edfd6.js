363e53248d2538ec608e5d93bcc1e108ae855d56
simplegr33n <gcgolda@gmail.com>
2020-10-06 23:00:41 -0600

    Updates

---
+++ precache-manifest.fdb0f1a129a8d75f92d5986f9c4edfd6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "3249e75742e30af431fd",
+    "url": "/grovv.xyz/static/css/main.71094d2e.chunk.css"
+  },
+  {
+    "revision": "3249e75742e30af431fd",
+    "url": "/grovv.xyz/static/js/main.0a45acaa.chunk.js"
+  },
+  {
+    "revision": "11f44ca7cbfb1074bb97",
+    "url": "/grovv.xyz/static/js/runtime~main.5f921949.js"
+  },
+  {
+    "revision": "36b89a461854cfab3507",
+    "url": "/grovv.xyz/static/css/2.0ff7e0ec.chunk.css"
+  },
+  {
+    "revision": "36b89a461854cfab3507",
+    "url": "/grovv.xyz/static/js/2.29d03132.chunk.js"
+  },
+  {
+    "revision": "5f290f46ef5af084f0d5cf7619eb0652",
+    "url": "/grovv.xyz/static/media/area.5f290f46.svg"
+  },
+  {
+    "revision": "41fa1b4fe58d1b41fd37412fef91e12a",
+    "url": "/grovv.xyz/static/media/container.41fa1b4f.svg"
+  },
+  {
+    "revision": "cb94dacb737c41f8509eeae568b6b017",
+    "url": "/grovv.xyz/index.html"
+  }
+];
