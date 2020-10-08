81a1de8bf9d0094a401b0e77f91ead0f906bb55f
simplegr33n <gcgolda@gmail.com>
2020-10-08 14:55:07 -0600

    more GrowDetailsPage clean up

--- src/App/main-content/GrowPage/GrowDetailsPage.js
+++ src/App/main-content/GrowPage/GrowDetailsPage.js
@@ -247,155 +247,7 @@ class GrowDetailsPage extends Component {
         }
     }
 
-
-
-    checkActive = (lastUpdateTime) => {
-
-        if (lastUpdateTime) {
-            var now = new Date();
-            var difference = now - (new Date(lastUpdateTime).getTime())
-
-            if (this._ismounted) {
-
-                if (difference >= 120000) {
-                    this.setState({
-                        activeIndicatorStyle: 'Grow-Active-Indicator-Circle Data-Warning-Background'
-                    });
-                }
-
-                if (difference >= 240000) {
-                    this.setState({
-                        activeIndicatorStyle: 'Grow-Active-Indicator-Circle Data-Danger-Background'
-                    });
-                }
-
-                if (difference < 120000) {
-                    this.setState({
-                        activeIndicatorStyle: 'Grow-Active-Indicator-Circle Data-Optimal-Background'
-                    });
-                }
-            }
-
-        } else {
-            console.log("NO LAST UPDATE ERROR")
-        }
-
-    }
-
-    openJournals = () => {
-        console.log("GROWDETAILS OPEN JOURNALS!")
-        if (this._ismounted) {
-            this.setState({
-                displayBottom: 'journals'
-            });
-        }
-    }
-    openData = () => {
-        console.log("GROWDETAILS OPEN DATA")
-        if (this._ismounted) {
-            this.setState({
-                displayBottom: 'data'
-            });
-        }
-    }
-    openConfig = () => {
-        console.log("GROWDETAILS OPEN CONFIG")
-        if (this._ismounted) {
-            this.setState({
-                displayBottom: 'config'
-            });
-        }
-    }
-    openFeed = () => {
-        if (this._ismounted) {
-            console.log("GROWDETAILS OPEN FEED")
-            this.setState({
-                displayBottom: 'feed'
-            });
-        }
-
-    }
-    openEditFeed = () => {
-        if (this._ismounted) {
-            console.log("GROWDETAILS OPEN EDIT FEED")
-            this.setState({
-                displayBottom: 'edit-feed'
-            });
-        }
-
-    }
-
-    openFullCam = (ev) => {
-        this.props.openFullCam(ev.target.dataset.value)
-    }
-
-    openJournal = (journal) => {
-        console.log("TODO: GrowDetailsPage open journal")
-        console.log(journal)
-        this.props.setJournalID(journal.id);
-    }
-
     render() {
-        //TODO: below in less code... or not using zIndex.
-        var zIndexOne = { zIndex: '1', position: 'absolute' }
-        var zIndexZero = { zIndex: '0', position: 'absolute' }
-
-        var journalsStyle = zIndexOne
-        var graphsStyle = zIndexZero
-        var feedStyle = zIndexZero
-        var editFeedStyle = zIndexZero
-        var configStyle = zIndexZero
-
-        if (this.state.displayBottom) {
-            switch (this.state.displayBottom) {
-                case 'journals':
-                    journalsStyle = zIndexOne
-                    graphsStyle = zIndexZero
-                    feedStyle = zIndexZero
-                    editFeedStyle = zIndexZero
-                    configStyle = zIndexZero
-                    break;
-                case 'data':
-                    journalsStyle = zIndexZero
-                    graphsStyle = zIndexOne
-                    feedStyle = zIndexZero
-                    editFeedStyle = zIndexZero
-                    configStyle = zIndexZero
-                    break;
-                case 'feed':
-                    journalsStyle = zIndexZero
-                    graphsStyle = zIndexZero
-                    feedStyle = zIndexOne
-                    editFeedStyle = zIndexZero
-                    configStyle = zIndexZero
-                    break;
-                case 'edit-feed':
-                    journalsStyle = zIndexZero
-                    graphsStyle = zIndexZero
-                    feedStyle = zIndexZero
-                    editFeedStyle = zIndexOne
-                    configStyle = zIndexZero
-                    break;
-                case 'config':
-                    journalsStyle = zIndexZero
-                    graphsStyle = zIndexZero
-                    feedStyle = zIndexZero
-                    editFeedStyle = zIndexZero
-                    configStyle = zIndexOne
-                    break;
-                default:
-                    break;
-            }
-        }
-
-        var renderedLinkedJournals = null;
-        if (this.state.linkedJournals.length !== 0) {
-            renderedLinkedJournals = this.state.linkedJournals.map((journal) =>
-                <div key={journal.id} className="Journal-Box-Item-Container">
-                    <JournalBoxItem journal={journal} openJournal={this.openJournal} />
-                </div>
-            )
-        }
 
         const listItems = this.state.SENSOR_PIDS.map((pid) =>
             <div className="Grow-Details-Main-Data-Display-Row">
@@ -556,52 +408,18 @@ class GrowDetailsPage extends Component {
 
                 <div className="Grow-Details-Page-Content">
 
-                    {/* <div className="Grow-Details-Bottom-Btns">
-                        <button className="Grow-Box-Function-Btn" data-value={'data'} onClick={this.openData} >DATA <span role="img" aria-label="grow data icon">&#128200;</span></button>
-                        <button className="Grow-Box-Function-Btn" data-value={'journals'} onClick={this.openJournals} >JRNLS <span role="img" aria-label="journal">&#128214;</span></button>
-                        <button className="Grow-Box-Function-Btn" data-value={'config'} onClick={this.openConfig} >CNFG <span role="img" aria-label="grow config icon">&#128187;</span></button>
-                        <button className="Grow-Box-Function-Btn-Feed" data-value={'feed'} onClick={this.openFeed} >FEED &#9619;&#9619;</button>
-                        <button className="Grow-Box-Function-Btn-Edit-Feed" data-value={'edit-feed'} onClick={this.openEditFeed} >&#9998;</button>
-                    </div> */}
-
                     <div className="Grow-Details-Content-Bottom">
-
-                        <div className="Grow-Details-Bottom-Item" style={journalsStyle} >
-                            <div className="Grow-Details-Journals">
-
-                                <div className="Grow-Details-Journals-Header">Connected Journals</div>
-                                <div style={{ overflowY: 'auto', display: 'flex', flexDirection: 'column', maxHeight: '100%' }}>
-                                    {renderedLinkedJournals}
-                                </div>
-
-                            </div>
-                        </div>
-
-                        <div className="Grow-Details-Bottom-Item" style={graphsStyle} >
-                            <GrowDetailsGraphs growID={this.props.grow.id} rawGrowData={this.props.rawGrowData} growConfig={this.state.growConfig} grow={this.props.grow} />
-                        </div>
-
-                        <div className="Grow-Details-Bottom-Item" style={feedStyle} >
-                            {/* <iframe id="Food-Chart" title="FoodChart" src={this.props.grow.urls.feed_chart} /> */}
-                        </div>
-
-                        <div className="Grow-Details-Bottom-Item" style={editFeedStyle} >
-                            {/* <object id="Edit-Food-Chart" type="text/html" data={this.props.grow.urls.feed_edit} width="100%" height="100%" aria-label="edit food chart" /> */}
-                        </div>
-
-                        <div className="Grow-Details-Bottom-Item" style={configStyle} >
-                            <GrowDetailsConfig growID={this.props.grow.id} />
-                        </div>
-
-                    </div>
-
-
-                    <div className="Grow-Details-Page-Panel">
                         <div className="Grow-Details-Header">
                             <div className={this.state.activeIndicatorStyle} />
                             <div id="Grow-Header-Text">{this.props.grow.name}</div>
                         </div>
+                        <div className="Grow-Details-Bottom-Item" >
+                            <GrowDetailsGraphs growID={this.props.grow.id} rawGrowData={this.props.rawGrowData} growConfig={this.state.growConfig} grow={this.props.grow} />
+                        </div>
+                    </div>
 
+
+                    <div className="Grow-Details-Page-Panel">
 
                         <div id="Grow-Details-Data-Display">
                             {(() => {
