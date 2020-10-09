4d7c3059c317e87c2b2ce7ef6bdddb39509eb4cb
simplegr33n <gcgolda@gmail.com>
2020-10-08 23:43:44 -0600

    Updates

---
+++ precache-manifest.f60522aed9db11d8699d9c17a5fa5b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "123f1283759c1705586e",
+    "url": "/grovv.xyz/static/css/main.0957a221.chunk.css"
+  },
+  {
+    "revision": "123f1283759c1705586e",
+    "url": "/grovv.xyz/static/js/main.e4ee1c9b.chunk.js"
+  },
+  {
+    "revision": "11f44ca7cbfb1074bb97",
+    "url": "/grovv.xyz/static/js/runtime~main.5f921949.js"
+  },
+  {
+    "revision": "d86aa4e3e5bfa4f5bc2c",
+    "url": "/grovv.xyz/static/css/2.0ff7e0ec.chunk.css"
+  },
+  {
+    "revision": "d86aa4e3e5bfa4f5bc2c",
+    "url": "/grovv.xyz/static/js/2.6dea3d2d.chunk.js"
+  },
+  {
+    "revision": "8bdb3c1624cc84353a8b2925c9baebfb",
+    "url": "/grovv.xyz/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/grovv.xyz/precache-manifest.6d50f1a2270d68949282ef5e2c20101f.js"
+  "/grovv.xyz/precache-manifest.f60522aed9db11d8699d9c17a5fa5b08.js"
 );
 
 workbox.clientsClaim();
