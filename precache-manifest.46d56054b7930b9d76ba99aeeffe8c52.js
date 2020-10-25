4d4055adaefe822e8f468e3ff1a03d21145af3cf
simplegr33n <gcgolda@gmail.com>
2020-10-24 21:10:06 -0600

    Updates

--- precache-manifest.46d56054b7930b9d76ba99aeeffe8c52.js
+++ precache-manifest.46d56054b7930b9d76ba99aeeffe8c52.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "cc599dd4ba4c83e6c72b",
+    "revision": "ec194a6e3a9503986827",
     "url": "/grovv.xyz/static/css/main.344791ec.chunk.css"
   },
   {
-    "revision": "cc599dd4ba4c83e6c72b",
-    "url": "/grovv.xyz/static/js/main.7ea74c67.chunk.js"
+    "revision": "ec194a6e3a9503986827",
+    "url": "/grovv.xyz/static/js/main.65311ca6.chunk.js"
   },
   {
     "revision": "11f44ca7cbfb1074bb97",
@@ -32,7 +32,7 @@ self.__precacheManifest = [
     "url": "/grovv.xyz/static/media/tobyface.95b324c2.svg"
   },
   {
-    "revision": "4de7f35cb689d164485ba4dfc97fdfb1",
+    "revision": "a837c6592d31dc4b565bfa81702c4b37",
     "url": "/grovv.xyz/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/grovv.xyz/precache-manifest.ee586ff70fab3f5aa46650c3db01c780.js"
+  "/grovv.xyz/precache-manifest.46d56054b7930b9d76ba99aeeffe8c52.js"
 );
 
 workbox.clientsClaim();
